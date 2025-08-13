cc8c0a34317e990f87b862c95b5bdf6eed7ba035
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2025-08-12 20:54:51 -0500

    Create Plugin.php

---
+++ src/Service/Composer/Plugin.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\Workbench\Service\Composer;
+
+use Composer\Composer;
+use Composer\IO\IOInterface;
+use Composer\Plugin\PluginInterface;
+use Override;
+
+final readonly class Plugin implements PluginInterface
+{
+    /**
+     * Apply plugin modifications to Composer.
+     */
+    #[Override]
+    public function activate(Composer $composer, IOInterface $io): void {}
+
+    /**
+     * Remove any hooks from Composer.
+     */
+    #[Override]
+    public function deactivate(Composer $composer, IOInterface $io): void {}
+
+    /**
+     * Prepare the plugin to be uninstalled.
+     */
+    #[Override]
+    public function uninstall(Composer $composer, IOInterface $io): void {}
+}
